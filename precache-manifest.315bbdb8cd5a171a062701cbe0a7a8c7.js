e28aab72c24472f1a5f945fe4ff2213a3a449b2a
chalosalvador <chalosalvador@gmail.com>
2020-07-12 18:22:03 -0500

    Updates

---
+++ precache-manifest.315bbdb8cd5a171a062701cbe0a7a8c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8698dc81a4b59de369812b467cf52fdd",
+    "url": "/pares-app/index.html"
+  },
+  {
+    "revision": "30f9dc3b36ea79c6bd31",
+    "url": "/pares-app/static/css/main.c0ef76ff.chunk.css"
+  },
+  {
+    "revision": "d1c5c12ef1a0b3ab5d69",
+    "url": "/pares-app/static/js/2.737effa2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pares-app/static/js/2.737effa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30f9dc3b36ea79c6bd31",
+    "url": "/pares-app/static/js/main.3074c3d5.chunk.js"
+  },
+  {
+    "revision": "f4a5f616a996a9a0aa52",
+    "url": "/pares-app/static/js/runtime-main.c0cb72e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pares-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
